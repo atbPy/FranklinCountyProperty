17884a41b916ee531bb8fc22ca15c9bd0ea1742a
adambednar <adam@bednar.co>
2022-08-14 16:52:58 -0400

    Work from Sunday

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ URL_DATE = current_date.strftime("%m/%d/%Y")
 FILE_DATE = current_date.strftime("%Y-%m-%d")
 
 # Download URL example https://audr-apps.franklincountyohio.gov/DailyConveyance/Home/DownloadCsv?StartDate=08/12/2022
-DOWNLOAD_URL = f"https://audr-apps.franklincountyohio.gov/DailyConveyance/Home/DownloadCsv?StartDate={URL_DATE}"
-DOWNLOAD_FILE = f"downloads/Conveyances-{FILE_DATE}.csv"
+# DOWNLOAD_URL = f"https://audr-apps.franklincountyohio.gov/DailyConveyance/Home/DownloadCsv?StartDate={URL_DATE}"
+# DOWNLOAD_FILE = f"downloads/Conveyances-{FILE_DATE}.csv"
+DOWNLOAD_URL = f"https://audr-apps.franklincountyohio.gov/DailyConveyance/Home/DownloadCsv?StartDate=08/12/2022"
+DOWNLOAD_FILE = "downloads/Conveyances-2022-08-12.csv"
 
 download_file(DOWNLOAD_FILE, DOWNLOAD_URL)
 
 process_file.open_and_read_file(DOWNLOAD_FILE)
+
 
 
 
--- process_file.py
+++ process_file.py
@@ -1,14 +1,32 @@
 import csv
+from database import mydatabase
+from datetime import datetime
+
 
 def open_and_read_file(DOWNLOAD_FILE):
+    dbms = mydatabase.MyDatabase(mydatabase.SQLITE, dbname='franklinCountyConveyances.sqlite')
     with open(DOWNLOAD_FILE, newline='') as csvfile:
+        next(csvfile)
         conveyances = csv.reader(csvfile, delimiter=',', quotechar='"')
-        row_count = (len(list(conveyances)))
 
-        if row_count > 1:
-            for row in conveyances:
-                print(len(row))
-                print(row)
-        else:
-            print("No records available to process")
+        for row in conveyances:
+            print(len(row))
+            print(row)
+            conveyance_record = {"conveyance_number": row[0],
+                                     "sale_date": datetime.strptime(row[2], '%Y-%m-%d'),
+                                     "sale_amount": row[3],
+                                     "sale_type": row[4],
+                                     "parcel_count": row[5],
+                                     "exempt": row[6],
+                                     "owner_name_1": row[7],
+                                     "owner_name_2": row[8],
+                                     "owner_address_1": row[9],
+                                     "owner_address_2": row[10],
+                                     "LUC": row[11],
+                                     "site_address": row[12]
+                                     }
+            print(conveyance_record)
+            dbms.insert_conveyance(conveyance_record)
 
+    dbms.print_all_data(mydatabase.CONVEYANCES)
+    csvfile.close()
